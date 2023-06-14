538e68d50d1cad16f403956f11f66243b78391d8
DeepakKannan1 <deepakjohn112000@gmail.com>
2023-06-14 17:46:36 +0530

    Multipage-E-Commerce

---
+++ Multipage-E-Commerce/script.js
@@ -0,0 +1,16 @@
+// const bar =document.getElementById("bar");
+// const nav =document.getElementById("navbar");
+// const close =document.getElementsByClassName("close");
+
+// if(bar){
+//     bar.addEventListener("click",function(){
+//         nav.classList.add("active");
+//         console.log("hi");
+//     });
+// }
+
+// if(close){
+//     close.addEventListener("click",()=>{
+//         nav.classList.remove("active");
+//     })
+//}
